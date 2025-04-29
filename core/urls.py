6e4fa1c0b5d6e39c9547aea3d406d2cacc032eaa
saint <damir.dzhubaliev@gmail.com>
2025-04-30 05:28:52 +0600

    added 5 new games and a player rating page

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
 	path('mastercards/', views.mastercards, name='mastercards'),
 	path('masterpieces/', views.masterpieces, name='masterpieces'),
 	path('genius_memory/', views.genius_memory, name='genius_memory'),
+	path('number_hunt/', views.number_hunt, name='number_hunt'),
+	path('color_pairs/', views.color_pairs, name='color_pairs'),
+	path('chess/', views.chess, name='chess'),
+	path('match_the_feel/', views.match_the_feel, name='match_the_feel'),
+	path('still_awake/', views.still_awake, name='still_awake'),
+	path('leaderboard/', views.leaderboard_view, name='leaderboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from users.models import CustomUser
 
 def index(request):
     return render(request, 'index.html')
@@ -27,9 +28,33 @@ def genius_memory(request):
     return render(request, 'genius_memory.html')
 
 @login_required
+def number_hunt(request):
+    return render(request, 'number_hunt.html')
+
+@login_required
+def color_pairs(request):
+    return render(request, 'color_pairs.html')
+
+@login_required
+def chess(request):
+    return render(request, 'chess.html')
+
+@login_required
+def match_the_feel(request):
+    return render(request, 'match_the_feel.html')
+
+@login_required
+def still_awake(request):
+    return render(request, 'still_awake.html')
+
+@login_required
 @require_POST
 def add_score(request):
     user = request.user
     user.score += 10  # Например, 10 очков за правильный ответ
     user.save()
     return JsonResponse({'status': 'success', 'new_score': user.score})
+
+def leaderboard_view(request):
+    users = CustomUser.objects.filter(is_active=True).order_by('-score')[:50]  # топ-50
+    return render(request, 'leaderboard.html', {'users': users})
